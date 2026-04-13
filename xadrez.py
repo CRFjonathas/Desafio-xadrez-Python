3bda5f83b1c10f7cb7630ca53615cd524c954806
CRFjonathas <jonaths.github@gmail.com>
2026-04-12 21:52:32 -0300

    feat: adciona movimento linear da torre e da rainha

---
+++ xadrez.py
@@ -0,0 +1,16 @@
+# FUNÇÕES
+
+def torre(casas = 5):
+    print("\n--- MOVIMENTO DA TORRE ---\n")
+    for c in range(casas):
+        print("DIREITA")
+
+def rainha(casas = 8):
+    print("\n--- MOVIMENTAÇÃO DA RAINHA ---\n")
+    for c in range(casas):
+        print("ESQUERDA")
+
+# PROGRAMA PRINCIPAL
+
+torre()
+rainha()
