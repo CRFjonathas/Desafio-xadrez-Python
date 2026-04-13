bb00483787ac2772c9730632870a20f93775a44d
CRFjonathas <jonaths.github@gmail.com>
2026-04-12 23:17:09 -0300

    feat: adiciona movimento em L do cavalo com loops aninhados

--- xadrez.py
+++ xadrez.py
@@ -10,7 +10,15 @@ def rainha(casas = 8):
     for c in range(casas):
         print("ESQUERDA")
 
+def cavalo(casas = 2):
+    print("\n--- MOVIMENTAÇÃO DO CAVALO ---\n")
+    for count in range(1):
+        for c in range(casas):
+            print("BAIXO")
+        print("ESQUERDA")
+
 # PROGRAMA PRINCIPAL
 
 torre()
 rainha()
+cavalo()
